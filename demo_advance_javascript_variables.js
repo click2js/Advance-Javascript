a160663491591817bd3cf4f350fa1875dd8aeb19
uilegends <uilegendsteam@gmail.com>
2022-11-27 21:29:00 +0530

    demo for var scope inside scope and functional scope

--- demo_advance_javascript_variables.js
+++ demo_advance_javascript_variables.js
@@ -1,1 +1,21 @@
-console.log("Hey");
+// Working with var for any scope
+
+var fname = "Ghan";
+if(true) {
+   var fname = "Alok";
+   console.log(fname, "name of inside scope");  // Alok name of inside scope
+}
+console.log(fname, "Out side of scope");  // Alok Out side of scope
+
+// Working with var for functional scope.
+
+var lname = "sharma";
+
+function demoVar() {
+    var lname = "panwar";
+    console.log(lname, "Inside functional scope"); // panwar
+}
+
+demoVar();
+console.log(lname, "Outside functional scope");  // sharma
+
