ff10d4f90a71ea15852cae2444910aab2704b802
uilegends <uilegendsteam@gmail.com>
2022-12-03 13:26:37 +0530

    arrow functions with example and printed each result

---
+++ arrow_function.js
@@ -0,0 +1,69 @@
+// simple no argument arrow function
+
+const myArr = _ => console.log("simple arrow function");
+myArr();
+
+// simple arrow function with single argument and for this no curly brakets are required.
+
+const singleArgu = x => console.log(x);
+singleArgu('Hello my function');
+
+// Arrow function with two arguments.
+
+const twoArgu = (x,y) => x+y;
+
+const twoarguResult = twoArgu(4,5);
+
+console.log(twoarguResult);
+
+// function with this keyword
+
+function Person() {
+    this.name = 'Jack', 
+    this.age = 25,
+    this.sayName  = function() {
+      console.log(this.age, "Priniting age inside outer function"); // this is accessable here 
+      function innerFunction() {
+        console.log(this.age);  // Here this referes to parent function not gloabal and this is defind in global class 
+      }
+      innerFunction();
+    }
+}
+
+const person = new Person();
+person.sayName();
+
+
+// Arrow function with this keyword
+
+function Person2() {
+    this.name = 'Bharat',
+    this.age = 23,
+    this.fullName = function() {
+        console.log(this.name, "Printing name");
+        const innerFunction = () => {
+           console.log(this.age, "Accessing this inside arrow function");  
+           // Here this refers to global object so (this.age) is accesable from global object.
+        }
+        return innerFunction();
+    }
+}
+
+const restPerson = new Person2();
+restPerson.fullName();
+
+
+// Arrow function not working inside object
+
+let people = {
+    name:"Peter",
+    age : 33,
+    profile: () => {
+     console.log(this.age); // Would be undefind as here this referes to global/window object 
+    }
+}
+
+people.profile();
+
+
+
